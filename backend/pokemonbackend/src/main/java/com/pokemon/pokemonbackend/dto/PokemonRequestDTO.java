08651d8585b11ecffc92358351c6656383829e93
JoelG <crastwtf@gmail.com>
2026-01-26 12:10:25 +0100

    Entrada de los DTO
    Código para la entrada de datos de los Pokémon

--- backend/pokemonbackend/src/main/java/com/pokemon/pokemonbackend/dto/PokemonRequestDTO.java
+++ backend/pokemonbackend/src/main/java/com/pokemon/pokemonbackend/dto/PokemonRequestDTO.java
@@ -1,4 +1,17 @@
 package com.pokemon.pokemonbackend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class PokemonRequestDTO {
+
+    @NotBlank
+    private String identifier; // nombre o número
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
 }
