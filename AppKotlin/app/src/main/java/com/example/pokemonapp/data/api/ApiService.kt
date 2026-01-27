55bc00af2ffa7c98f2c37cfe209c775a782361a2
JoelG <crastwtf@gmail.com>
2026-01-27 09:29:20 +0100

    Update ApiService.kt

--- AppKotlin/app/src/main/java/com/example/pokemonapp/data/api/ApiService.kt
+++ AppKotlin/app/src/main/java/com/example/pokemonapp/data/api/ApiService.kt
@@ -1,2 +1,46 @@
 package com.example.pokemonapp.data.api
 
+import com.example.pokemonapp.data.model.*
+import retrofit2.http.*
+
+interface ApiService {
+
+    // -------- USERS --------
+
+    @GET("users")
+    suspend fun getUsers(): List<UserDto>
+
+    @POST("users")
+    suspend fun createUser(@Body user: Map<String, String>): UserDto
+
+    // -------- TEAMS --------
+
+    @GET("teams/user/{userId}")
+    suspend fun getTeamsByUser(
+        @Path("userId") userId: Long
+    ): List<TeamDto>
+
+    @POST("teams")
+    suspend fun createTeam(
+        @Query("userId") userId: Long,
+        @Body team: Map<String, String>
+    ): TeamDto
+
+    // -------- POKEMON --------
+
+    @GET("pokemon/team/{teamId}")
+    suspend fun getPokemonByTeam(
+        @Path("teamId") teamId: Long
+    ): List<PokemonDto>
+
+    @POST("pokemon")
+    suspend fun addPokemon(
+        @Query("teamId") teamId: Long,
+        @Body request: Map<String, String>
+    ): PokemonDto
+
+    @DELETE("pokemon/{id}")
+    suspend fun deletePokemon(
+        @Path("id") pokemonId: Long
+    )
+}
