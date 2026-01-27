3c88087e85685dde054bc19462e44d576a7d50fd
HectorMarBaeDev <hectormarbaeza@gmail.com>
2026-01-27 10:36:57 +0100

    Terminada la LoginScreen y el NavGraph

--- AppKotlin/app/src/main/java/com/example/pokemonapp/navigation/NavGraph.kt
+++ AppKotlin/app/src/main/java/com/example/pokemonapp/navigation/NavGraph.kt
@@ -1,2 +1,38 @@
 package com.example.pokemonapp.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pokemonapp.ui.login.LoginScreen
+
+@Composable
+fun NavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        composable("login") {
+            LoginScreen(
+                onUserSelected = { userId ->
+                    navController.navigate("teams/$userId")
+                }
+            )
+        }
+
+        composable (
+            route = "teams/{userId}",
+            arguments = listOf(
+                navArgument("userId") { type = NavType.LongType}
+            )
+        ) {
+            backStackEntry ->
+            val userId = backStackEntry.arguments!!.getLong("teamId")
+            TeamDetailScreen(teamId = teamId)
+        }
+    }
+}
--- AppKotlin/app/src/main/java/com/example/pokemonapp/ui/login/LoginScreen.kt
+++ AppKotlin/app/src/main/java/com/example/pokemonapp/ui/login/LoginScreen.kt
@@ -1,7 +1,13 @@
 package com.example.pokemonapp.ui.login
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -12,6 +18,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.pokemonapp.data.model.UserDto
 import com.example.pokemonapp.data.repository.PokemonRepository
 
@@ -31,11 +39,33 @@ fun LoginScreen( onUserSelected: (Long) -> Unit ) {
 
     Scaffold(
         topBar = {
-            TopAppBar(title = {
-                Text("Selecciona usuario")
-            })
+            TopAppBar(title = { Text("Selecciona usuario") })
         }
-    ) {
-        padding -> Column()
+    ) { padding ->
+
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            users.forEach { users ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp)
+                        .clickable {
+                            onUserSelected(user.id)
+                        }
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Text(user.username, style = MaterialTheme.typography.titleMedium)
+                        Text(user.email, style = MaterialTheme.typography.bodyMedium)
+                    }
+                }
+            }
+        }
     }
 }
