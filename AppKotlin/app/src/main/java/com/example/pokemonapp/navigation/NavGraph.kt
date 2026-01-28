4268f12f49a82a10f27201abff73eb2db3b940b7
AlvaroLopezArnal <sacraboix@gmail.com>
2026-01-28 09:37:11 +0100

    NavGraph Updated
    Hemos solucionado errores

--- AppKotlin/app/src/main/java/com/example/pokemonapp/navigation/NavGraph.kt
+++ AppKotlin/app/src/main/java/com/example/pokemonapp/navigation/NavGraph.kt
@@ -7,15 +7,20 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.pokemonapp.ui.login.LoginScreen
+import com.example.pokemonapp.ui.teams.TeamListScreen
+import com.example.pokemonapp.ui.teams.TeamDetailScreen
 
 @Composable
 fun NavGraph() {
+
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
         startDestination = "login"
     ) {
+
+        // -------- LOGIN --------
         composable("login") {
             LoginScreen(
                 onUserSelected = { userId ->
@@ -24,14 +29,33 @@ fun NavGraph() {
             )
         }
 
-        composable (
+        // -------- LISTA DE EQUIPOS --------
+        composable(
             route = "teams/{userId}",
             arguments = listOf(
-                navArgument("userId") { type = NavType.LongType}
+                navArgument("userId") { type = NavType.LongType }
             )
-        ) {
-            backStackEntry ->
-            val userId = backStackEntry.arguments!!.getLong("teamId")
+        ) { backStackEntry ->
+
+            val userId = backStackEntry.arguments!!.getLong("userId")
+
+            TeamListScreen(
+                userId = userId,
+                onTeamClick = { teamId ->
+                    navController.navigate("team/$teamId")
+                }
+            )
+        }
+
+        // -------- DETALLE DE EQUIPO --------
+        composable(
+            route = "team/{teamId}",
+            arguments = listOf(
+                navArgument("teamId") { type = NavType.LongType }
+            )
+        ) { backStackEntry ->
+
+            val teamId = backStackEntry.arguments!!.getLong("teamId")
             TeamDetailScreen(teamId = teamId)
         }
     }
