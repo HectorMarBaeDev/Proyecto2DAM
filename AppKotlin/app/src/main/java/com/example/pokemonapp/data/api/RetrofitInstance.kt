9fd6f8bb49bcc749d573b148b3537274d2512603
HectorMarBaeDev <hectormarbaeza@gmail.com>
2026-01-29 12:03:33 +0100

    Intento de conexion de la api con la aplicacion movil

--- AppKotlin/app/src/main/java/com/example/pokemonapp/data/api/RetrofitInstance.kt
+++ AppKotlin/app/src/main/java/com/example/pokemonapp/data/api/RetrofitInstance.kt
@@ -6,8 +6,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
 
     //private const val BASE_URL = "http://10.0.2.2:8080/api/"
-    private const val BASE_URL = "http://localhost:8080/api/"
-
+    //private const val BASE_URL = "http://localhost:8080/api/"
+    private const val BASE_URL = "https://pokemon-backend-849x.onrender.com/api"
     //private const val BASE_URL = "http://192.168.56.1:8080/api/"
 
     val api: ApiService by lazy {
