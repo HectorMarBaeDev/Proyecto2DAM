6451280dc17dbde5a30247959fd7f9ff4a665141
brandon <hectormarbaeza@gmail.com>
2026-02-16 13:28:46 +0100

    más ajustes de swagger

--- backend/pokemonbackend/src/main/java/com/pokemon/pokemonbackend/security/WebSecurityConfig.java
+++ backend/pokemonbackend/src/main/java/com/pokemon/pokemonbackend/security/WebSecurityConfig.java
@@ -44,6 +44,10 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/health").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-resources/**").permitAll()
+                        .requestMatchers("/webjars/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
